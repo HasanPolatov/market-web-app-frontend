8ee3174991c342f6455cdbfe068d77b35357cd95
Hasan <hasanpolatov@gmail.com>
2023-06-05 04:19:34 +0500

    - Add styles for login div (simple)

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -24,8 +24,8 @@ const login = async () => {
 </script>
 
 <template>
-  <div>
-    <label for="username">Login</label>
+  <div class="login-form">
+    <label for="username">Username</label>
     <input type="text" name="username" v-model="authLogin.username">
     <label for="password">Password</label>
     <input type="password" name="password" v-model="authLogin.password">
@@ -34,5 +34,33 @@ const login = async () => {
 </template>
 
 <style scoped>
+
+.login-form {
+  width: 400px;
+  margin: 80px auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+}
+
+.login-form label {
+  font-size: 16px;
+  font-weight: bold;
+}
+
+.login-form input {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+}
+
+.login-form button {
+  background-color: #04AA6D;
+  color: white;
+  font-size: 16px;
+  font-weight: bold;
+  padding: 10px;
+  border: none;
+  cursor: pointer;
+}
 
 </style>
